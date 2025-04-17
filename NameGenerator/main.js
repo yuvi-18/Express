d65b954321050fe570f693a6937025afb3d7b1a9
Yuvraj <fcyuvraj18@gmail.com>
2025-04-17 15:16:55 +0530

    Update main.js

--- NameGenerator/main.js
+++ NameGenerator/main.js
@@ -3,17 +3,19 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req ,res)=>{
-  res.sendFile('/public/form.html', {root: __dirname})
+app.get('/', (req, res) => {
+  res.sendFile('/public/form.html', { root: __dirname })
 })
 
-app.post('/submit', (req ,res)=>{
-  const obj = req.body
+app.post('/submit', (req, res) => {
+  const obj = req.body;
+  const array = Object.values(obj);
+  const result = array[0] + array[1];
   res.send(`
     <h1>Your Band Name is:</h1>
-    <h2>${obj['street name']}${obj['pet name']} 😛</h2>
+    <h2>${result}😛</h2>
     `)
 })
 
